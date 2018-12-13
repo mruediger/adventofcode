c73f875b4963abdfc8494b1c7a9e43ea8fd0a807
Mathias Rüdiger <ruediger@blueboot.org>
2018-12-13 10:09:24 +0100

    add day3

---
+++ day3/src/claim.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+#[derive(Clone)]
+pub struct Claim {
+        id: u32,
+        x: u32,
+        y: u32,
+        w: u32,
+        h: u32
+    }
+
+impl Claim {
+    pub fn from_line(line: String) -> Claim {
+        // #1 @ 509,796: 18x15
+
+        let elements: Vec<&str> = line.split(' ').collect();
+
+        let id = elements.get(0).unwrap()
+            .trim_left_matches('#')
+            .parse().unwrap();
+
+        let cordinates: Vec<u32> = elements.get(2).unwrap()
+            .trim_right_matches(':')
+            .split(',').map(|s| s.parse().unwrap()).collect();
+
+        let size: Vec<u32> = elements.get(3).unwrap()
+            .split('x').map(|s| s.parse().unwrap()).collect();
+
+        Claim {
+            id: id,
+            x: *cordinates.get(0).unwrap(),
+            y: *cordinates.get(1).unwrap(),
+            w: *size.get(0).unwrap(),
+            h: *size.get(0).unwrap()
+        }
+    }
+
+    pub fn intersection(&self, claim: &Claim) -> Option<Claim> {
+        None
+    }
+
+    pub fn intersections(&self, claims: &Vec<Claim>) -> Vec<Option<Claim>> {
+        claims.iter()
+            .filter(|claim| claim.id != self.id)
+            .map(|claim| self.intersection(claim))
+            .collect()
+    }
+}
+
+impl PartialEq for Claim {
+    fn eq(&self, other: &Claim) -> bool {
+        self.id == other.id
+    }
+}
+impl Eq for Claim {}
---
+++ day3/src/main.rs
@@ -0,0 +1,29 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+mod claim;
+
+fn intersections(claim: &claim::Claim, claims: &Vec<claim::Claim>) -> Vec<claim::Claim> {
+    Vec::new()
+}
+
+fn main() -> std::io::Result<()>{
+    let f = File::open("../input.txt")?;
+    let reader = BufReader::new(f);
+
+    let claims: Vec<claim::Claim> = reader
+        .lines()
+        .flatten()
+        .map(claim::Claim::from_line)
+        .collect();
+
+    let intersections = claims.iter()
+        .flat_map(|c| c.intersections(&claims))
+        .flatten();
+
+
+    intersections.for_each(|i| println!("{:?}", i));
+
+    Ok(())
+}
