1451a863106e7d3cf70b282f036b998aabce3ca3
Mathias Rüdiger <ruediger@blueboot.org>
2018-12-19 11:21:17 +0100

    nix funktioniert

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,29 +1,44 @@
-use std::fs::File;
-use std::io::BufRead;
-use std::io::BufReader;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
 mod claim;
+mod cloth;
 
-fn intersections(claim: &claim::Claim, claims: &Vec<claim::Claim>) -> Vec<claim::Claim> {
-    Vec::new()
-}
+static INPUT: &str = include_str!("../input.txt");
+pub type Cloth = Vec<Vec<u8>>;
+fn main() {
+    let claims: Vec<claim::Claim> = INPUT.lines().map(claim::Claim::from_line).collect();
 
-fn main() -> std::io::Result<()> {
-    let f = File::open("../input.txt")?;
-    let reader = BufReader::new(f);
+    let (w, h) = claims.iter().fold((0, 0), |acc, claim| {
+        (
+            std::cmp::max(acc.0, claim.l + claim.w + 1),
+            std::cmp::max(acc.1, claim.t + claim.h + 1),
+        )
+    });
 
-    let claims: Vec<claim::Claim> = reader
-        .lines()
-        .flatten()
-        .map(claim::Claim::from_line)
-        .collect();
+    let mut cloth = cloth::Cloth::new(w as usize, h as usize);
+    claims.iter().for_each(|claim| cloth.mark(claim));
 
-    let intersections = claims
-        .iter()
-        .flat_map(|c| c.intersections(&claims))
-        .flatten();
+    println!("{} square inches are claimed", cloth.count_claimed());
 
-    intersections.for_each(|i| println!("{:?}", i));
+    let mut unintersected: Vec<claim::Claim> = Vec::new();
 
-    Ok(())
+    let first = claims[0].clone();
+
+    println!("first claim: {:?}", first);
+
+    for claim in claims {
+        if claim.intersects(&first) {
+            unintersected.push(claim.clone())
+        }
+    }
+
+    for x in (0..w) {
+        for y in (0..w) {
+            claims
+                .filter(|claim| !claim.contains_point(x, y))
+                .for_each(|claim| unintersected.push(claim))
+        }
+    }
 }
