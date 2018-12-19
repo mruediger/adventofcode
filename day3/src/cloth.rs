b0598a82e18f2e497e40469fddf3dda4f8200a64
Mathias Rüdiger <ruediger@blueboot.org>
2018-12-19 19:51:56 +0100

    it works but is not beautiful

--- day3/src/cloth.rs
+++ day3/src/cloth.rs
@@ -1,20 +1,27 @@
 use super::claim;
 
-pub struct Cloth {
-    data: Vec<Vec<u32>>,
+pub struct Cloth<'a> {
+    data: Vec<Vec<Vec<&'a claim::Claim>>>,
 }
 
-impl Cloth {
-    pub fn new(w: usize, h: usize) -> Cloth {
+impl<'a> Cloth<'a> {
+    pub fn new() -> Cloth<'a> {
         Cloth {
-            data: vec![vec![0; h]; w],
+            data: vec![vec![Vec::new(); 1]; 1],
         }
     }
 
-    pub fn mark(&mut self, claim: &claim::Claim) {
-        for x in (0..claim.w).map(|i| i + claim.l) {
-            for y in (0..claim.h).map(|i| i + claim.t) {
-                self.data[x as usize][y as usize] += 1;
+    pub fn mark(&mut self, claim: &'a claim::Claim) {
+        for x in (0..claim.w as usize).map(|i| i + claim.l as usize) {
+            for y in (0..claim.h as usize).map(|i| i + claim.t as usize) {
+                if self.data.len() < x + 1 {
+                    self.data.resize(x + 1, vec![Vec::new(); 1])
+                }
+
+                if self.data[x].len() < y + 1 {
+                    self.data[x].resize(y + 1, Vec::new())
+                }
+                self.data[x][y].push(&claim)
             }
         }
     }
@@ -23,7 +30,17 @@ impl Cloth {
         self.data
             .iter()
             .flatten()
-            .map(|c| if c > &1 { 1 } else { 0 })
+            .map(|c| if c.len() > 1 { 1 } else { 0 })
             .sum()
+    }
+
+    pub fn contested_ids(&self) -> Vec<u32> {
+        self.data
+            .iter()
+            .flatten()
+            .filter(|c| c.len() > 1)
+            .flatten()
+            .map(|c| c.id)
+            .collect()
     }
 }
