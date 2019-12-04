88bdc2bbc4c6aa94dc4b09f18ce94339ed19ad0e
Mathias Rüdiger <ruediger@blueboot.org>
2019-12-04 10:17:45 +0100

    nice, but stolen solution

--- 2019/day3/src/main.rs
+++ 2019/day3/src/main.rs
@@ -1,170 +1,58 @@
-#[macro_use]
-extern crate itertools;
-
+use std::collections::HashSet;
 use std::fs;
 
 const INPUT: &str = "../input.txt";
 
-#[derive(Clone, Copy, Debug)]
-struct Point {
-    x: i32,
-    y: i32,
+fn parse_step(step: &str) -> impl Iterator<Item = (isize, isize)> {
+    let direction = step.get(0..1).unwrap();
+    let distance = step.get(1..).unwrap().parse().unwrap();
+
+    let coord = match direction {
+        "U" => (0, -1),
+        "D" => (0, 1),
+        "L" => (-1, 0),
+        "R" => (1, 0),
+        _ => (0, 0),
+    };
+
+    std::iter::repeat(coord).take(distance)
 }
 
-#[derive(Clone, Copy, Debug)]
-struct Line {
-    a: Point,
-    b: Point,
-    steps: i32,
+fn parse_line(line: &str) -> impl Iterator<Item = (isize, isize)> + '_ {
+    line.split(",")
+        .flat_map(parse_step)
+        .scan((0, 0), |pos, step| {
+            pos.0 += step.0;
+            pos.1 += step.1;
+            Some(pos.clone())
+        })
 }
 
-impl Line {
-    pub fn is_horizontal(&self) -> bool {
-        self.a.y == self.b.y
-    }
-
-    pub fn is_vertical(&self) -> bool {
-        self.a.x == self.b.x
-    }
-
-    pub fn normalized(&self) -> Line {
-        if self.is_horizontal() {
-            if self.a.x < self.b.x {
-                Line {
-                    a: self.a,
-                    b: self.b,
-                    steps: self.steps,
-                }
-            } else {
-                Line {
-                    a: self.b,
-                    b: self.a,
-                    steps: self.steps,
-                }
-            }
-        } else {
-            if self.a.y < self.b.y {
-                Line {
-                    a: self.a,
-                    b: self.b,
-                    steps: self.steps,
-                }
-            } else {
-                Line {
-                    a: self.b,
-                    b: self.a,
-                    steps: self.steps,
-                }
-            }
-        }
-    }
-}
-
-struct Wire {
-    lines: Vec<Line>,
-}
-
-impl Wire {
-    pub fn new(steps: Vec<&str>) -> Wire {
-        let mut lines: Vec<Line> = Vec::new();
-        let mut previous = Point { x: 0, y: 0 };
-
-        for step in steps {
-            let direction = step.get(0..1).unwrap();
-            let distance = step.get(1..).unwrap().parse::<i32>().unwrap();
-
-            let next = match direction {
-                "R" => Some(Point {
-                    x: previous.x + distance,
-                    y: previous.y,
-                }),
-                "D" => Some(Point {
-                    x: previous.x,
-                    y: previous.y - distance,
-                }),
-                "L" => Some(Point {
-                    x: previous.x - distance,
-                    y: previous.y,
-                }),
-                "U" => Some(Point {
-                    x: previous.x,
-                    y: previous.y + distance,
-                }),
-                _ => None,
-            }
-            .unwrap();
-
-            lines.push(Line {
-                a: previous,
-                b: next,
-                steps: lines.last().map_or(0, |l| l.steps) + distance,
-            });
-            previous = next;
-        }
-
-        Wire { lines: lines }
-    }
-}
-
-fn intersection(line1: &Line, line2: &Line) -> Option<(Point, Line, Line, i32)> {
-    if line1.is_horizontal() && line2.is_vertical() {
-        if line2.normalized().a.y < line1.a.y
-            && line2.normalized().b.y > line1.b.y
-            && line1.normalized().a.x < line2.a.x
-            && line1.normalized().b.x > line2.b.x
-        {
-            let l1_steps = line1.steps - (line1.normalized().b.x - line1.normalized().a.x)
-                + (line2.b.x - line1.normalized().a.x);
-            let l2_steps = line2.steps - (line2.normalized().b.y - line2.normalized().a.y)
-                + (line1.b.y - line2.normalized().a.y);
-
-            return Some((
-                Point {
-                    x: line2.a.x,
-                    y: line1.a.y,
-                },
-                line1.clone(),
-                line2.clone(),
-                l1_steps + l2_steps,
-            ));
-        }
-    }
-
-    if line1.is_vertical() && line2.is_horizontal() {
-        return intersection(line2, line1);
-    }
-
-    None
-}
-
-fn distance(a: Point, b: Point) -> i32 {
-    (a.x - b.x).abs() + (a.y - b.y).abs()
+fn manhattan_distance(pos: &(isize, isize)) -> usize {
+    pos.0.abs() as usize + pos.1.abs() as usize
 }
 
 fn main() {
     let input = fs::read_to_string(INPUT).unwrap();
 
-    //let input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-    //U98,R91,D20,R16,D67,R40,U7,R15,U6,R7";
+    let mut lines = input.lines().map(parse_line);
 
-    let wires = input
-        .lines()
-        .map(|l| l.split(",").collect())
-        .map(Wire::new)
-        .collect::<Vec<Wire>>();
+    let steps_0: Vec<(isize, isize)> = lines.next().unwrap().collect();
+    let steps_1: Vec<(isize, isize)> = lines.next().unwrap().collect();
 
-    let min = iproduct!(wires[0].lines.iter(), wires[1].lines.iter())
-        .map(|(a, b)| intersection(a, b))
-        .flatten()
-        .map(|i| distance(Point { x: 0, y: 0 }, i.0))
+    let positions_0: HashSet<_> = steps_0.iter().cloned().collect();
+    let positions_1: HashSet<_> = steps_1.iter().cloned().collect();
+    let collisions = positions_0.intersection(&positions_1);
+
+    let closest_collision = collisions.clone().map(manhattan_distance).min();
+    println!("Part One: {:?}", closest_collision.unwrap());
+
+    let quickest_collision = collisions
+        .map(|pos| {
+            steps_0.iter().position(|x| x == pos).unwrap()
+                + steps_1.iter().position(|x| x == pos).unwrap()
+                + 2
+        })
         .min();
-
-    println!("{:?}", min.unwrap());
-
-    let min_steps = iproduct!(wires[0].lines.iter(), wires[1].lines.iter())
-        .map(|(a, b)| intersection(a, b))
-        .flatten()
-        .min_by(|a, b| a.3.cmp(&b.3));
-
-    println!("{:?}", min_steps)
+    println!("Part Two: {:?}", quickest_collision.unwrap());
 }
