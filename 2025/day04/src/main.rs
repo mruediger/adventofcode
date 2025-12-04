fa9755af3b9216bde4e58f3f4af72ec2a6ed9729
Mathias Rüdiger <ruediger@blueboot.org>
2025-12-04 23:01:51 +0100

    day04 done

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -2,6 +2,7 @@ mod input;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -15,6 +16,40 @@ fn part1() {
         .count();
 
     println!("part 1: {}", movable);
+}
+
+fn part2() {
+    let mut input = input::InputMap::from_file("../input");
+
+    let mut movable: Vec<(usize,usize)> = input.iter_positions()
+        .filter(|(x,y)| input.get(*x,*y) == '@')
+        .filter(|(x,y)| {
+            input.get_adjacent_values(*x,*y).iter().filter(|c| **c == '@').count() < 4
+        })
+        .collect();
+
+    let mut total = 0;
+
+    while !movable.is_empty() {
+        total += movable.len();
+
+        for (x,y) in &movable {
+            input.set(*x,*y,'x')
+        }
+
+        for (x,y) in &movable {
+            input.set(*x,*y,'.')
+        }
+
+        movable = input.iter_positions()
+        .filter(|(x,y)| input.get(*x,*y) == '@')
+        .filter(|(x,y)| {
+            input.get_adjacent_values(*x,*y).iter().filter(|c| **c == '@').count() < 4
+        })
+        .collect();
+    }
+
+    println!("part 2: {}", total);
 }
 
 #[test]
@@ -66,4 +101,36 @@ fn test_sample_input2() {
                                 @.@.@@@.@."};
 
     let mut input = input::InputMap::new(INPUT);
+
+    let mut movable: Vec<(usize,usize)> = input.iter_positions()
+        .filter(|(x,y)| input.get(*x,*y) == '@')
+        .filter(|(x,y)| {
+            input.get_adjacent_values(*x,*y).iter().filter(|c| **c == '@').count() < 4
+        })
+        .collect();
+
+    let mut total = 0;
+
+    while movable.len() > 0 {
+        total += movable.len();
+
+        for (x,y) in &movable {
+            input.set(*x,*y,'x')
+        }
+
+        println!("{}", input);
+
+        for (x,y) in &movable {
+            input.set(*x,*y,'.')
+        }
+
+        movable = input.iter_positions()
+        .filter(|(x,y)| input.get(*x,*y) == '@')
+        .filter(|(x,y)| {
+            input.get_adjacent_values(*x,*y).iter().filter(|c| **c == '@').count() < 4
+        })
+        .collect();
+    }
+
+    println!("{}", total);
 }
