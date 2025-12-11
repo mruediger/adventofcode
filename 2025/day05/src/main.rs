80ad86213b55e20a295d95b41a34f7e99b551699
Mathias Rüdiger <ruediger@blueboot.org>
2025-12-11 17:35:18 +0100

    day05 finished

--- 2025/day05/src/main.rs
+++ 2025/day05/src/main.rs
@@ -1,107 +1,78 @@
-    use std::collections::HashSet;
-
-
-struct IDDatabase {
-    ranges: Vec<(u64,u64)>,
-}
-
-impl IDDatabase {
-
-
-    fn new(input: &str) -> Self {
-        let data:Vec<(u64,u64)> = input.lines().map(|l| l.split_once("-").expect("a string containing -"))
-            .map(|(start,end)| (start.parse().unwrap(), end.parse().unwrap()))
-            .collect();
-        IDDatabase{ ranges: data }
-    }
-
-    fn is_fresh(&self, id: u64) -> bool {
-        self.ranges.iter().any(|(start,end)| id >= *start && id <= *end)
-    }
-
-    fn ranges_count(self) -> usize {
-        let mut ids:Vec<u64> = self.ranges.into_iter().flat_map(|(start, end)| start..=end).collect();
-        ids.sort();
-        ids.dedup();
-        ids.len()
-    }
-}
-
-
 fn main() {
     let input = std::fs::read_to_string("../input")
         .expect("input file");
 
-    let (id_ranges, ingredients) =
-        input.split_once("\n\n")
-        .expect("id_ranges and ingredients");
-
-    let ids = IDDatabase::new(id_ranges);
-    let count = ingredients.lines()
-        .flat_map(|l| l.parse::<u64>())
-        .filter(|id| ids.is_fresh(*id))
-        .count();
-    println!("part 1 {}", count);
-    println!("part 2 {}", ids.ranges_count());
+    println!("part 1: {}", part1(&input));
+    println!("part 2: {}", part2(&input));
 }
 
-#[test]
-fn test_part1() {
 
-    use indoc::indoc;
+fn parse(input:&str) -> (Vec<(u64,u64)>,Vec<u64>) {
+    let (ranges, ingredients) = input.split_once("\n\n").unwrap();
 
-    const INPUT: &str = indoc!{"3-5
-                                10-14
-                                16-20
-                                12-18
+    let ranges: Vec<(u64,u64)> = ranges.lines().map(|s| {
+        let (first,last) = s.split_once("-").unwrap();
+        (first.parse().unwrap(), last.parse().unwrap())
+    }).collect();
 
-                                1
-                                5
-                                8
-                                11
-                                17
-                                32"};
+    let ingredients: Vec<u64> = ingredients.lines().map(|s| s.parse().unwrap()).collect();
 
+    (ranges,ingredients)
+}
 
+fn part1(input:&str) -> usize {
+    let (ranges, ingredients) = parse(input);
 
-    if let Some((id_ranges, ingredients)) = INPUT.split_once("\n\n") {
-        let ids = IDDatabase::new(id_ranges);
-        ingredients.lines()
-            .flat_map(|l| l.parse::<u64>())
-            .for_each(|i| println!("{} is fresh? {}", i, ids.is_fresh(i)));
+    ingredients
+        .iter()
+        .filter(|id| ranges.iter().any(|(start,end)| *id >= start && *id <= end))
+        .count()
+}
+
+fn part2(input:&str) -> usize {
+    let (mut ranges,_) = parse(input);
+
+    ranges.sort_by_key(|(start,_)| *start);
+
+    let mut merged:Vec<(u64,u64)> = vec![*ranges.first().unwrap()];
+
+    for range in ranges.iter().skip(1) {
+        let prev = *merged.last().unwrap();
+        if range.0 > prev.1 {
+            merged.push(*range);
+        } else {
+            merged.pop();
+            merged.push((prev.0, prev.1.max(range.1)))
+        }
     }
+
+    merged.iter().flat_map(|(start,end)| *start..=*end).count()
 }
 
-
-#[test]
-fn test_part2() {
-
+mod test {
     use indoc::indoc;
-    use std::collections::HashSet;
 
-    const INPUT: &str = indoc!{"3-5
-                                10-14
-                                16-20
-                                12-18
+    const CASE: &str = indoc! {"
+        3-5
+        10-14
+        16-20
+        12-18
 
-                                1
-                                5
-                                8
-                                11
-                                17
-                                32"};
+        1
+        5
+        8
+        11
+        17
+        32
+    "};
 
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(CASE), 3);
+    }
 
-
-    if let Some((id_ranges, ingredients)) = INPUT.split_once("\n\n") {
-        let ids = IDDatabase::new(id_ranges);
-
-
-        let mut seen = HashSet::new();
-
-        ids.ranges.into_iter().flat_map(|(start, end)| start..=end)
-            .filter(|i| seen.insert(*i));
-
-
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(CASE), 14);
     }
 }
